8342646d4abd168812a5406455ce8b925277f253
Nathaniel Flores <nathanielflores@Nathaniels-MacBook-Pro.local>
2016-06-22 21:17:58 -0500

    adding styling and proper routing

--- js/script.js
+++ js/script.js
@@ -7,36 +7,51 @@ var BASE_URL = "https://api.forecast.io/forecast/"
 //var geolocatorApiKey = "AIzaSyBvkVytYncPHjeEXjwN_mHRtBQtQRkULfk"
 //var geolocatorBaseUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key="
 
+var STATE = {
+	lat: null,
+	lng: null
+}
 
 
-var weatherContainerNode = document.querySelector("#weatherAppContainer")
-var buttonContainerNode = document.querySelector("#nav-buttons") 
-var dailyWeatherNode = document.querySelector("#dailyWeatherContainer")
-var hourlyWeatherNode = document.querySelector("#hourlyWeatherContainer")
+
+var weatherContainerNode = document.querySelector("#weatherContainer")
+var buttonContainerNode = document.querySelector("#buttonContainer") 
+
+var addAllEventListeners = function () {
+	var handleViewTypeChange = function (eventObject) {
+
+		var buttonNode = eventObject.target,
+			viewType = buttonNode.value
+
+			location.hash = STATE.lat + "/" + STATE.lng + "/" + viewType
+
+	}
+
+		buttonContainerNode.addEventListener('click', handleViewTypeChange)
+
+}
 
 var renderCurrentView = function (weatherApiResponse) {
-	console.log('current view being called')
-	var weatherObj = weatherApiResponse
-	console.log(weatherObj)
-	var htmlString = " "
-	var temperature = weatherObj.currently.temperature,
-		icon = weatherObj.currently.icon,
-		apparentTemp = weatherObj.currently.apparentTemperature
 
-	var degreeSymbol = "°"
-	degreeSymbol = degreeSymbol.substr(1)
+		var weatherObj = weatherApiResponse
+		var temperature = weatherObj.currently.temperature,
+			icon = weatherObj.currently.icon,
+			apparentTemp = weatherObj.currently.apparentTemperature
 
-	htmlString += "<h3>" + "FORCAST NOW" + "</h3>"
-	htmlString += "<div class = 'forcastCards'>"
-	htmlString += "<p class = 'temp'>" + parseInt(temperature) + degreeSymbol +  "</p>"
-	htmlString += "<p>" + "FEELS LIKE " + parseInt(apparentTemp) + degreeSymbol + "</p>"
-	htmlString += "<p>" + icon + "</p>"
-	htmlString += "</div>"
+		var degreeSymbol = "°"
+		degreeSymbol = degreeSymbol.substr(1)
 
+		var htmlString = ""
+
+		htmlString += "<div class = 'currentForcastCards'>"
+		htmlString += "<p>" + parseInt(temperature) + degreeSymbol +  "</p>"
+		htmlString += "<p>" + "FEELS LIKE " + parseInt(apparentTemp) + degreeSymbol + "</p>"
+		htmlString += "<p>" + icon + "</p>"
+		htmlString += "</div>"
 
 
-	var currentWeatherContainer = document.querySelector("#weatherAppContainer")
-	currentWeatherContainer.innerHTML = htmlString
+
+	weatherContainerNode.innerHTML = htmlString
 }
 
 
@@ -46,7 +61,8 @@ var renderDailyView  = function (weatherApiResponse) {
 	var weatherObj = weatherApiResponse
 	var dailyWeatherArray = weatherObj.daily.data
 
-	htmlString = "<h3>" + "FORCAST DAILY" + "</h3>"
+	var htmlString = ""
+
 	for(var i = 0; i < dailyWeatherArray.length; i++) {
 		var dailyWeatherObj = dailyWeatherArray[i]
 
@@ -82,17 +98,16 @@ var renderDailyView  = function (weatherApiResponse) {
 
 		var weatherDateString = getDayOfTheWeek(weekdayNumber) + " " + dayOfTheMonthNumber
 
-		htmlString += "<div class = 'forcastCards'>"		
-		htmlString += "<p id = 'time'>" + weatherDateString + "</p>"
-		htmlString += "<p class = 'temp'>" + parseInt(dailyWeatherObj.temperatureMax) + degreeSymbol + "</p>"
-		htmlString += "<p>" + summary + "</p>"
-		htmlString += "</div>"
-		
 	
 
-
-		var dailyWeatherContainer = document.querySelector("#weatherAppContainer")
-		dailyWeatherContainer.innerHTML = htmlString
+			htmlString += "<div class = 'dailyForcastCards'>"		
+			htmlString += "<p id = 'time'>" + weatherDateString + "</p>"
+			htmlString += "<p class = 'temp'>" + parseInt(dailyWeatherObj.temperatureMax) + degreeSymbol + "</p>"
+			htmlString += "<p>" + summary + "</p>"
+			htmlString += "</div>"
+		
+	
+		weatherContainerNode.innerHTML = htmlString
 	}
 
 }
@@ -100,25 +115,23 @@ var renderDailyView  = function (weatherApiResponse) {
 
 
 var renderHourlyView = function (weatherApiResponse) {
-	console.log(weatherApiResponse)
+
 	var weatherObj = weatherApiResponse
-	console.log(weatherObj)
 	var hourlyWeatherArray = weatherObj.hourly.data
-	//console.log(hourlyWeatherArray)
 
-	
-		htmlString = "<h3>" + "HOURLY FORCAST TOMORROW" + "</h3>" 	
+
+		var htmlString = ""
+		
 		for(var i = 0; i < hourlyWeatherArray.length; i++) {
-		var hourlyWeatherObj = hourlyWeatherArray[i]
-
-		var degreeSymbol = "°"
-		degreeSymbol = degreeSymbol.substr(1)
+			var hourlyWeatherObj = hourlyWeatherArray[i]
+			var degreeSymbol = "°"
+			degreeSymbol = degreeSymbol.substr(1)
 
 		
 		var toDateTimeSecs = function(secs) {
-	    var t = new Date(1970, 0, 1); // Epoch
-	    t.setSeconds(secs);
-	    return t;
+		    var t = new Date(1970, 0, 1); // Epoch
+		    t.setSeconds(secs);
+		    return t;
 		}
 
 		var summary = hourlyWeatherObj.summary
@@ -134,42 +147,41 @@ var renderHourlyView = function (weatherApiResponse) {
 	    return hours + amPm;
 		};
 
-		htmlString += "<div class = 'forcastCards'>"
-		htmlString += "<p id = 'hourTime'>" + standardHr(militaryHr) + "</p>"
-		htmlString += "<p class = 'temp'>" + parseInt(hourlyWeatherObj.temperature) + degreeSymbol + "</p>"
-		htmlString += "<p>" + summary + "</p>"
-		htmlString += "</div>"
+
+			htmlString += "<div class = 'hourlyForcastCards'>"
+			htmlString += "<p id = 'hourTime'>" + standardHr(militaryHr) + "</p>"
+			htmlString += "<p class = 'temp'>" + parseInt(hourlyWeatherObj.temperature) + degreeSymbol + "</p>"
+			htmlString += "<p>" + summary + "</p>"
+			htmlString += "</div>"
 
 
-		var hourlyWeatherContainer = document.querySelector("#weatherAppContainer")
-		hourlyWeatherContainer.innerHTML = htmlString
-		
+		weatherContainerNode.innerHTML = htmlString
 	}
 
 }
 
-var hashChange = function(eventObj) {
 
-		var viewType = eventObj.target.value
-		location.hash = lat + "/" + lng + "/" + viewType
+var geoLocate = function() { 
 
-}
+		var successFunc = function (geoPosResponse) {
 
-buttonContainerNode.addEventListener('click', hashChange)
-
-
-var errorHandler = function(error) {
-	console.log(error)
-}
-
-
-var defaultLocation = function(geoPosResponse) { 
+			//console.log("I'm the current location", geoPosResponse)
 		
-		var lat = geoPosResponse.coords.latitude
-		var lng = geoPosResponse.coords.longitude
-		location.hash = lat + "/" + lng + "/" + "current"
+			var lat = geoPosResponse.coords.latitude,
+				lng = geoPosResponse.coords.longitude
 
+				STATE.lat = lat
+				STATE.lng = lng
+			
+				location.hash = lat + "/" + lng + "/" + "current"
+				//console.log(location.hash)
+				
+		}
+
+		navigator.geolocation.getCurrentPosition(successFunc, function(err){console.log(err)})
 }
+
+
 
 
 
@@ -196,19 +208,13 @@ var WeatherRouter = Backbone.Router.extend ({
 		":lat/:lng/current": "showCurrentView",
 		":lat/:lng/daily": "showDailyView",
 		":lat/:lng/hourly": "showHourlyView",
-		"*default": "geolocate"
+		"*default": "handleDefault"
 
 	},
 
-
-
-
-	geolocate: function () {
-		//get the current latitude and longitude of the computer
-		navigator.geolocation.getCurrentPosition(defaultLocation, errorHandler)
-		//write them to the hash, appending "current" as the default view
-		
-		//location.hash = "25/95/current"
+	handleDefault: function () {
+		//console.log("Hey I'm getting the POS")
+		geoLocate()
 	},
 
 
@@ -218,17 +224,24 @@ var WeatherRouter = Backbone.Router.extend ({
 		// make a request for forecast data at the input lat and lng
 		// 
 		// queues up renderCurrentView to run when the request is fulfilled
+		
+		STATE.lat = lat
+		STATE.lng = lng
+		
 
 		var promise = doWeatherRequest(lat, lng)
 		promise.then(renderCurrentView)
 		
 
-		// 
+	
 		// doWeatherRequest(lat,lng)//>>>>do .then request
 
 	},
 
 	showDailyView: function (lat, lng) {
+
+		STATE.lat = lat
+		STATE.lng = lng
 
 		var promise = doWeatherRequest(lat, lng)
 		promise.then(renderDailyView)
@@ -236,57 +249,21 @@ var WeatherRouter = Backbone.Router.extend ({
 	},
 
 	showHourlyView: function (lat, lng) {
+
+		STATE.lat = lat
+		STATE.lng = lng
 		
 		var promise = doWeatherRequest(lat, lng)
 		promise.then(renderHourlyView)
 
+	},
+
+	initialize: function () {
+		Backbone.history.start()// tell backbone to start watching the hash and tracking browser history
+		addAllEventListeners()
 	}
 
 })
 
 // create a new instance of the router
 var rtr = new WeatherRouter()
-
-// tell backbone to start watching the hash and tracking browser history
-Backbone.history.start()
-
-/*
-var controller = function () {
-	
-	if (!location.hash) {
-
-		navigator.geolocation.getCurrentPosition(defaultLocation, errorHandler)
-		return
-	
-	}
-
-	var hashObj = getHashData()
-	
-
-	var weatherPromise = $.getJSON(BASE_URL + APIKEY + "/" + hashObj.lat + "," + hashObj.lng)
-
-	if (hashObj.viewType === "current") {
-		
-		weatherPromise.then(renderCurrentView)
-	
-	} else if (hashObj.viewType === "daily") {
-			
-		weatherPromise.then(renderDailyView)
-
-	} else if (hashObj.viewType === "hourly") {
-			
-		weatherPromise.then(renderHourlyView)
-	}	
-	
-
-}
-
-
-controller() //>>> why did you have to call the controller function
-
-window.addEventListener('hashchange', controller)  
-
-
-//var searchBar = document.querySelector("input")
-*/
-//searchBar.addEventListener("keydown", searchFunction)

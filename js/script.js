4d736bd238f2e767de1f39fc4af12a0956c7337d
Nathaniel Flores <nathanielflores@Nathaniels-MacBook-Pro.local>
2016-09-01 00:45:05 -0500

    add skycons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 //forcast api = "https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE"
 //geolocator api = "https://www.googleapis.com/geolocation/v1/geolocate?key="
-//
+
 var APIKEY = "ad05a86902b581dcb64cbfacef513319"
 var BASE_URL = "https://api.forecast.io/forecast/"
 
 //var geolocatorApiKey = "AIzaSyBvkVytYncPHjeEXjwN_mHRtBQtQRkULfk"
 //var geolocatorBaseUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key="
 
+var geocodingToken = "AIzaSyCdofWTwoOaNmKWEIvyvbcTaLItlJNuerg"
+var geocodingUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="
+
 var STATE = {
 	lat: null,
 	lng: null
 }
 
-
+var searchedObj = {
+	name: 'Houston'
+}
 
 var weatherContainerNode = document.querySelector("#weatherContainer")
 var buttonContainerNode = document.querySelector("#buttonContainer") 
+var searchContainerNode = document.querySelector("#searchContainer")
 
 var addAllEventListeners = function () {
 	var handleViewTypeChange = function (eventObject) {
@@ -28,6 +34,35 @@ var addAllEventListeners = function () {
 	}
 
 		buttonContainerNode.addEventListener('click', handleViewTypeChange)
+		searchContainerNode.addEventListener('keydown', getSearchCoordinates)
+}
+
+var fetchCoordinates = function (address) {
+	var locationUrl = geocodingUrl + address + '&' + geocodingToken
+	var locationPromise = $.getJSON(locationUrl)
+	return locationPromise
+
+}
+
+var getSearchCoordinates = function (eventObj) {
+	if(eventObj.keyCode === 13){
+		var city = eventObj.target.value
+		var coordinatesPromise = fetchCoordinates(city)
+		coordinatesPromise.then(showCurrentCity)
+		eventObj.target.value = ''
+
+	}
+
+}
+
+var showCurrentCity = function (localeObj) {
+	var inputLat = localeObj.results[0].geometry.location.lat
+	var inputLng = localeObj.results[0].geometry.location.lng
+	location.hash = inputLat + '/' + inputLng + '/current'
+
+	var formattedAddress = localeObj.results[0].formatted_address.split(",")
+	var locationName = formattedAddress[0]
+	searchedObj.name = locationName
 
 }
 
@@ -44,6 +79,7 @@ var renderCurrentView = function (weatherApiResponse) {
 		var htmlString = ""
 
 		htmlString += "<div class = 'currentForcastCards'>"
+		htmlString += "<p>" + searchedObj.name + "</p>"
 		htmlString += "<p>" + parseInt(temperature) + degreeSymbol +  "</p>"
 		htmlString += "<p>" + "FEELS LIKE " + parseInt(apparentTemp) + degreeSymbol + "</p>"
 		htmlString += "<p>" + icon + "</p>"
@@ -112,10 +148,9 @@ var renderDailyView  = function (weatherApiResponse) {
 
 }
 
-
-
 var renderHourlyView = function (weatherApiResponse) {
 
+	console.log(weatherApiResponse)
 	var weatherObj = weatherApiResponse
 	var hourlyWeatherArray = weatherObj.hourly.data
 
@@ -124,49 +159,43 @@ var renderHourlyView = function (weatherApiResponse) {
 		
 		for(var i = 0; i < hourlyWeatherArray.length; i++) {
 			var hourlyWeatherObj = hourlyWeatherArray[i]
+			
+			var milliSeconds = new Date(hourlyWeatherObj.time * 1000)
+
+			var formatAMPM = function(date) {
+				  var hours = date.getHours();
+				  var minutes = date.getMinutes();
+				  var ampm = hours >= 12 ? 'PM' : 'AM';
+				  hours = hours % 12;
+				  hours = hours ? hours : 12;
+				  var strTime = hours + ' ' + ampm;
+				  return strTime;
+			
+			}
+			
+			var hourStr = formatAMPM(milliSeconds)
+			var summary = hourlyWeatherObj.summary
 			var degreeSymbol = "°"
 			degreeSymbol = degreeSymbol.substr(1)
 
-		
-		var toDateTimeSecs = function(secs) {
-		    var t = new Date(1970, 0, 1); // Epoch
-		    t.setSeconds(secs);
-		    return t;
-		}
-
-		var summary = hourlyWeatherObj.summary
-		var getHours = toDateTimeSecs(hourlyWeatherObj.time).getHours()
-		var militaryHr = getHours.toString(10)
-
-		var standardHr = function (twoDigitTime) {
-	    var hours24 = parseInt(twoDigitTime.substring(0, 2),10);
-	    var hours = ((hours24 + 11) % 12) + 1;
-	    var amPm = hours24 > 11 ? 'pm' : 'am';
-  
-
-	    return hours + amPm;
-		};
-
-
 			htmlString += "<div class = 'hourlyForcastCards'>"
-			htmlString += "<p id = 'hourTime'>" + standardHr(militaryHr) + "</p>"
+			htmlString += "<p id = 'hourTime'>" + hourStr  + "</p>"
 			htmlString += "<p class = 'temp'>" + parseInt(hourlyWeatherObj.temperature) + degreeSymbol + "</p>"
 			htmlString += "<p>" + summary + "</p>"
 			htmlString += "</div>"
 
-
+	
+		};
 		weatherContainerNode.innerHTML = htmlString
 	}
 
-}
 
 
 var geoLocate = function() { 
 
 		var successFunc = function (geoPosResponse) {
 
-			//console.log("I'm the current location", geoPosResponse)
-		
+			console.log(location)
 			var lat = geoPosResponse.coords.latitude,
 				lng = geoPosResponse.coords.longitude
 
@@ -174,7 +203,7 @@ var geoLocate = function() {
 				STATE.lng = lng
 			
 				location.hash = lat + "/" + lng + "/" + "current"
-				//console.log(location.hash)
+				
 				
 		}
 
@@ -188,17 +217,14 @@ var geoLocate = function() {
 
 var doWeatherRequest = function (lat, lng) {
 	//https://api.forecast.io/forecast/
-	//console.log(lat,lng)
 
 	var weatherFullUrl = BASE_URL + APIKEY + "/" + lat + "," + lng 
 	weatherFullUrl.substr(1)
 	var weatherPromise = $.getJSON(weatherFullUrl)
 	// weatherPromise.then(function (data) {console.log(data) })
+	
 	return weatherPromise
 }
-
-
-
 
 
 var WeatherRouter = Backbone.Router.extend ({
@@ -213,8 +239,8 @@ var WeatherRouter = Backbone.Router.extend ({
 	},
 
 	handleDefault: function () {
-		//console.log("Hey I'm getting the POS")
 		geoLocate()
+
 	},
 
 
@@ -222,7 +248,6 @@ var WeatherRouter = Backbone.Router.extend ({
 	showCurrentView: function (lat, lng) {
 
 		// make a request for forecast data at the input lat and lng
-		// 
 		// queues up renderCurrentView to run when the request is fulfilled
 		
 		STATE.lat = lat
@@ -232,8 +257,6 @@ var WeatherRouter = Backbone.Router.extend ({
 		var promise = doWeatherRequest(lat, lng)
 		promise.then(renderCurrentView)
 		
-
-	
 		// doWeatherRequest(lat,lng)//>>>>do .then request
 
 	},
@@ -260,7 +283,8 @@ var WeatherRouter = Backbone.Router.extend ({
 
 	initialize: function () {
 		Backbone.history.start()// tell backbone to start watching the hash and tracking browser history
-		addAllEventListeners()
+		addAllEventListeners() 
+
 	}
 
 })
